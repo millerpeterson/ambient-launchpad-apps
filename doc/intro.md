82a9129b21143a172de0a38846bd46edb0592dee
Miller Peterson <miller.peterson@gmail.com>
2014-11-19 21:36:22 -0500

    Lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ambient-launchpad-apps
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ambient_launchpad_apps/core_test.clj
@@ -0,0 +1,7 @@
+(ns ambient-launchpad-apps.core-test
+  (:require [clojure.test :refer :all]
+            [ambient-launchpad-apps.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
