1c180e55d1ceb3b637fc6f5ce0d6e744e5bc97e3
Miller Peterson <miller.peterson@gmail.com>
2015-01-03 21:19:55 -0500

    Setting leds, filling regions

--- src/sprite_assembly/op.clj
+++ src/sprite_assembly/op.clj
@@ -1,8 +1,20 @@
 (ns sprite-assembly.op
-  (require [sprite-assembly.sprite :as sprite]))
+  (require [sprite-assembly.sprite :as sprite]
+           [sprite-assembly.led :as led]))
 
 (defn region-map
   "Map leds in a given region of a sprite."
-  [spr region func]
-  (reduce #(assoc %1 %2 (func %2 (get %1 %2)))
-          spr region))
+  ([spr func & {:keys [region] :or {region sprite/positions}}]
+    (reduce #(assoc %1 %2 (func %2 (get %1 %2)))
+      spr region)))
+
+(defn set-led
+  "Set an individual led."
+  ([spr r c colr]
+    (region-map spr (fn [_ _] (vec colr)) :region [[r c]])))
+
+(defn fill
+  "Fill a region with a specific color."
+  ([spr color & {:keys [region] :or {region sprite/positions}}]
+    (region-map spr (fn [_ _] (vec color)) :region region)))
+
