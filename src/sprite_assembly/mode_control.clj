067ab4c9d379d23545a838b66d5ba5fed2432f59
Miller Peterson <miller.peterson@gmail.com>
2015-07-07 01:05:22 -0400

    Refactor mode change handler a bit

--- src/sprite_assembly/mode_control.clj
+++ src/sprite_assembly/mode_control.clj
@@ -3,24 +3,33 @@
             [sprite-assembly.buttons :as btn]))
 
 (def mode-change-buttons
+  "Modes mapping to the button position that changes to them."
   {:assembly [0 4]
    :frames [0 5]
    :draw [0 6]
    :palette [0 7]})
 
 (def mode-change-positions
+  "Button positions that change mode."
   (into #{} (vals mode-change-buttons)))
 
+(defn mode-for-position
+  "Mode that corresponds to a given mode-change button position."
+  [position]
+  (let [[row col] position]
+    ((set/map-invert mode-change-buttons) [row col])))
+
 (defn change-mode
+  "Return a model with its mode changed to new-mode."
   [new-mode model]
   (assoc model :active-mode new-mode))
 
 (defn handler
+  "Change mode when a mode-change button is pressed."
   [event model]
   (let [[row col _] event]
     (cond (and (btn/press? event)
                (contains? mode-change-positions [row col]))
-          (let [new-mode ((set/map-invert mode-change-buttons) [row col])]
-            (change-mode new-mode model))
+          (change-mode (mode-for-position [row col]) model)
           :else model)))
 
